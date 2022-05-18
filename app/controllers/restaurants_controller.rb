1461742c18aa1a1e3e74c1abeee26f4823b1a191
Ted Andre <elleted@hotmail.com>
2022-05-18 15:42:51 +0200

    implemeting reviews to our restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,6 +18,27 @@ class RestaurantsController < ApplicationController
     redirect_to restaurants_path
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if
+    @restaurant.update(restaurant_params)
+      redirect_to restaurants_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    redirect_to restaurants_path
+  end
+
   private
 
   def restaurant_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
