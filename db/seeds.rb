9aa88354e03df4b0544c8ab42f461782642058fa
Ted Andre <elleted@hotmail.com>
2022-05-18 14:56:23 +0200

    new action implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w(chinese italian japanese french belgian).sample
+  )
+  restaurant.save!
+end
