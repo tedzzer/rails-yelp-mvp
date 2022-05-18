60adde1d5cafd9fde69fe09afa000e7d2b1d8678
Ted Andre <elleted@hotmail.com>
2022-05-18 12:12:25 +0200

    validation done for restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+  has_many :reviews
 end
---
+++ db/migrate/20220518095318_change_columns_from_restaurants.rb
@@ -0,0 +1,6 @@
+class ChangeColumnsFromRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :restaurants, :adress, :address
+    change_column :restaurants, :phone_number, :string
+  end
+end
